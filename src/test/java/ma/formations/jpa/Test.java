bdec1f8861e99ee324bc5c0f595d9e21640d8225
FloofHips <redjoker351@gmail.com>
2024-05-24 13:24:27 +0100

    Include initial hashing to Test

--- src/test/java/ma/formations/jpa/Test.java
+++ src/test/java/ma/formations/jpa/Test.java
@@ -1,5 +1,9 @@
 package ma.formations.jpa;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 import ma.formations.jpa.dao.DaoImplJPA;
 import ma.formations.jpa.dao.IDao;
 import ma.formations.jpa.dao.article.ArticleDaoImplJPA;
@@ -11,11 +15,11 @@ public class Test {
 	public static void main(String[] args) {
 		IDao dao = new DaoImplJPA();
 		dao.deleteAll();
-		dao.save(User.builder().username("user01").password("user01").build());
-		dao.save(User.builder().username("user02").password("user02").build());
-		dao.save(User.builder().username("user03").password("user03").build());
-		dao.save(User.builder().username("user04").password("user04").build());
-		dao.save(User.builder().username("user05").password("user05").build());
+		dao.save(User.builder().username("user01").password(hashPassword("user01")).build());
+		dao.save(User.builder().username("user02").password(hashPassword("user02")).build());
+		dao.save(User.builder().username("user03").password(hashPassword("user03")).build());
+		dao.save(User.builder().username("user04").password(hashPassword("user04")).build());
+		dao.save(User.builder().username("user05").password(hashPassword("user05")).build());
 		IArticleDao daoArticle = new ArticleDaoImplJPA();
 		daoArticle.deleteAll();
 		daoArticle.save(Article.builder().description("PC PRTABLE HP").price(30D).quantite(15000d).build());
@@ -24,5 +28,15 @@ public class Test {
 		daoArticle.save(Article.builder().description("TV SONY").price(10.0).quantite(8500.0).build());
 		daoArticle.save(Article.builder().description("CAMERA SONY").price(12.0).quantite(2500.0).build());
 		daoArticle.save(Article.builder().description("DD 100G").price(27.0).quantite(500.0).build());
+	}
+	
+	private static String hashPassword(String password) {
+		try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashedPassword = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashedPassword);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password.", e);
+        }
 	}
 }
